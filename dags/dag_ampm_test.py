3ec6d9a755ed5e3cae0751a1cad20b4b6b02f3a5
jinwoo <sabrikong@gmail.com>
2024-03-14 10:34:13 +0900

    first upload

---
+++ dags/dag_ampm_test.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.email import EmailOperator
+
+with DAG(
+    dag_id="dag_ampm_test.py",
+    schedule="30 6-9, 17-21 * * 1-5",
+    start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    send_email_task = EmailOperator(
+        task_id='send_email_task',
+        to='gescheit00@naver.com',
+        subject='ampm test',
+        html_content='ampm test.'
+    )
