50aa3c4cd407e685fafaa2a042732762abdf92d6
jinwoo <sabrikong@gmail.com>
2024-06-01 22:31:48 +0900

    test

--- dags/weather_current.py
+++ dags/weather_current.py
@@ -62,13 +62,13 @@ def load(**kwargs):
     schema = 'dev.adhoc'
     table = 'WEATHER_CURRENT'
     
-    insert_sql = f"INSERT INTO {schema}.{table} VALUE ({city}, {weather_condition}, {created_at})"
+    insert_sql = f"INSERT INTO {schema}.{table} VALUES ({city}, {weather_condition}, {created_at})"
     logging.info(insert_sql)
     try:
         cur.execute(insert_sql)
         cur.execute("COMMIT;")
     except Exception as e:
-        cur.excute("ROLLBACK;")
+        cur.execute("ROLLBACK;")
         raise e
 
 with DAG(
