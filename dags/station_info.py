006bd9bf8a557c0728d3b5932545ccec6971eca4
jinwoo <sabrikong@gmail.com>
2024-06-02 14:20:50 +0900

    test

--- dags/station_info.py
+++ dags/station_info.py
@@ -15,6 +15,14 @@ def get_Redshift_connection(autocommit=False):
 
 @task(task_id='py_etl')
 def get_station_info():
+    import os
+
+    #현재 폴더 경로; 작업 폴더 기준
+    print(os.getcwd())
+
+    #현재 파일의 폴더 경로; 작업 파일 기준
+    print(os.path.dirname(os.path.realpath(__file__)))
+
     df = pd.read_csv("/home/airflow/dataset/station_info.csv")
     insert_sql = ''
     schema = 'dev.adhoc'
