d6c8b2b1913aa13f7bfe769086d20ff666103bb7
jinwoo <sabrikong@gmail.com>
2024-06-03 06:02:30 +0900

    timeinterval

--- dags/get_bus_arrival_item.py
+++ dags/get_bus_arrival_item.py
@@ -123,7 +123,7 @@ def load(**kwargs):
 
 with DAG(
     dag_id="get_bus_arrival_item",
-    schedule="*/1 6-9,17-20 * * *",
+    schedule="*/1 6-9 * * *",
     start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False,
     tags=['bus'],
